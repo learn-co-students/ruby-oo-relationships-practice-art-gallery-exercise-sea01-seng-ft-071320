99e2f5a6456491452d7b300a5cac6b337db212af
afteralec <github email address>
2020-07-21 22:24:25 -0700

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,36 @@
 class Gallery
-
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
-    @name = name
-    @city = city
+    @name, @city = name, city
+    
+    self.save
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Instance methods that fetch data
+  def paintings
+    Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artists
+    paintings.map(&:artist)
+  end
+
+  def artist_names
+    artists.map(&:name)
+  end
+
+  def most_expensive_painting
+    paintings.max_by(&:price)
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
-
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist
+  attr_accessor :gallery
 
   @@all = []
 
-  def initialize(title, price)
-    @title = title
-    @price = price
+  def initialize(title, price, artist, gallery=nil)
+    @title, @price, @artist, @gallery = title, price, artist, gallery
+    
+    self.save
   end
 
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Class methods that aggregate data
+  def self.total_price
+    all.map(&:price).sum
+  end
 end
